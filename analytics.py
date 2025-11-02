dbfc8ba1abb14dfb8c4778577ddd6c4b634efbbf
Zamalevskiy <9725999@gmail.com>
2025-11-02 22:31:24 +0300

    Update analytics.py

--- analytics.py
+++ analytics.py
@@ -1,14 +1,22 @@
+import os
+import json
 import gspread
-from oauth2client.service_account import ServiceAccountCredentials
+from google.oauth2.service_account import Credentials
 import datetime
 
 # Подключение к Google Sheets
 def connect_to_sheet():
-    scope = ["https://spreadsheets.google.com/feeds",
-             "https://www.googleapis.com/auth/drive"]
-    creds = ServiceAccountCredentials.from_json_keyfile_name("celestial-baton-477016-k2-a0724e20f278.json", scope)
-    client = gspread.authorize(creds)
-    sheet = client.open("Bot-analitic").sheet1  # замените на название таблицы
+    # Чтение JSON-содержимого сервисного аккаунта из переменной окружения
+    json_content = os.environ["GOOGLE_SERVICE_ACCOUNT_JSON"]
+    credentials_dict = json.loads(json_content)
+
+    # Создание Credentials и подключение к gspread
+    creds = Credentials.from_service_account_info(credentials_dict)
+    client = gspread.Client(auth=creds)
+    client.login()
+
+    # Открываем таблицу по названию
+    sheet = client.open("Bot-analitic").sheet1
     return sheet
 
 # Функция логирования события
@@ -16,3 +24,4 @@ def log_event(user_id, username, section_id, button_id, next_section):
     sheet = connect_to_sheet()
     timestamp = datetime.datetime.now().isoformat()
     sheet.append_row([user_id, username, timestamp, section_id, button_id, next_section])
+
