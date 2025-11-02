84fc64e28f212f9df28cbf71b185ac81118fd54f
Zamalevskiy <9725999@gmail.com>
2025-11-02 23:21:09 +0300

    Update analytics.py
    НОВЫЙ КЛЮЧ

--- analytics.py
+++ analytics.py
@@ -7,13 +7,15 @@ import datetime
 # Подключение к Google Sheets
 def connect_to_sheet():
     # Чтение JSON-содержимого сервисного аккаунта из переменной окружения
-    json_content = os.environ["GOOGLE_SERVICE_ACCOUNT_JSON"]
+    json_content = os.environ.get("GOOGLE_SERVICE_ACCOUNT_JSON")
+    if not json_content:
+        raise ValueError("Переменная окружения GOOGLE_SERVICE_ACCOUNT_JSON не найдена")
+
     credentials_dict = json.loads(json_content)
 
     # Создание Credentials и подключение к gspread
     creds = Credentials.from_service_account_info(credentials_dict)
     client = gspread.Client(auth=creds)
-    client.login()
 
     # Открываем таблицу по названию
     sheet = client.open("Bot-analitic").sheet1
@@ -24,4 +26,3 @@ def log_event(user_id, username, section_id, button_id, next_section):
     sheet = connect_to_sheet()
     timestamp = datetime.datetime.now().isoformat()
     sheet.append_row([user_id, username, timestamp, section_id, button_id, next_section])
-
