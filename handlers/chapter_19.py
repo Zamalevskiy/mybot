71694b06123e3625b8a382357ab681d9108e35dd
Zamalevskiy <9725999@gmail.com>
2025-11-03 14:37:51 +0300

    Update chapter_19.py

--- handlers/chapter_19.py
+++ handlers/chapter_19.py
@@ -1,12 +1,22 @@
 import asyncio
 from aiogram import Router, types, F
 from aiogram.utils.keyboard import InlineKeyboardBuilder
+from analytics import log_event
 
 router = Router()
 
 
 @router.callback_query(F.data == "chapter_19")
 async def chapter_19_handler(callback: types.CallbackQuery):
+    # Логирование нажатия кнопки
+    log_event(
+        user_id=callback.from_user.id,
+        username=callback.from_user.username or "",
+        action_type="button_click",
+        action_name="chapter_19",
+        additional_data=""
+    )
+    
     # Сообщение 1
     text1 = (
         "<b>Оплати по СБП или Переводом по номеру карты.</b>\n\n"
